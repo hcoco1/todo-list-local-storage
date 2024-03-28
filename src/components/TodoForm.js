3c66173509608e8d3a760c1d2ca97f2f6cdc01c1
Ivan Arias <arias.ivan@gmail.com>
2024-03-27 20:59:26 -0400

    form updated

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -82,17 +82,18 @@ function TodoForm({ addTodo, newTodo, setNewTodo }) {
       
 
       <FormSelect
-            value={newTodo.coaching}
             name="coaching"
+            value={newTodo.coaching}
             onChange={handleInputChange}
             options={[
               { label: 'Coaching', value: '' },
               { label: 'Induct', value: "The auditor coached the associate, focusing on item shortages, scanning inaccuracies, placement errors, and the mishandling of damaged goods. The coaching emphasized enhancing observation, adhering to the 'one piece flow' principle for scanning accuracy, ensuring precise item placement in trays, and promptly reporting damaged items." },
               { label: 'Induct Trays issues', value: "The auditor coached the associate, focusing on the crucial steps of accurately scanning items and ensuring their correct placement in trays. Accurate placement is critical for maintaining operational efficiency and preventing errors and missing items." },
               { label: 'Rebin', value: "The associate was coached explicitly on the importance of carefully verifying chute IDs against the screen instructions before placement, emphasizing a methodical approach over speed to ensure accuracy." },
-              { label: 'Pack', value: "The team undertook a comprehensive audit of the Pack process to evaluate overall efficiency and accuracy, focusing on critical stages, including box assembly, item scanning, placement, and the final steps of sealing and labeling packages." },
+              { label: 'Pack General', value: "The team undertook a comprehensive audit of the Pack process to evaluate overall efficiency and accuracy, focusing on critical stages, including box assembly, item scanning, placement, and the final steps of sealing and labeling packages." },
               { label: 'Pack Missing', value: "The team initiated an audit focusing on the procedures for locating missing items (COIN Method). The associate was coached in checking multiple potential locations for missing items, including the floor, alternative chutes, and within the PS (Problem Solve) tote." },
               { label: 'Pack Damaged', value: "The associate was coached regarding the handling and reporting procedures for damaged items to assess the integrity and effectiveness of standard work protocols." },
+              { label: 'Pack Wrong Box', value: "The team noted that the associate needed to match the recommended box size (displayed on the screen) with the actual size. Clear and concise instructions regarding box size matching were reiterated to the associate through an audit. The employee was reminded of the potential consequences of non-compliance, including increased kick-outs and operational disruptions." },
               { label: 'Pack Unscannable', value: "The associate was coached to improve the procedures for managing unscannable items, with a particular emphasis on implementing a 'six-side check' by associates on each item and a thorough inspection to verify the presence of a scannable barcode, thereby identifying potential scanning impediments." },
               { label: 'Pack Shipment Exceptions', value: "The associate was coached on the importance of promptly addressing shipment exceptions, like slip printer malfunctions, that can delay the packing process" },
               { label: 'Slam Kickout', value: "The associate faced an audit for repeated kick-outs related to incorrect label placements, including hazmat and spoon labels. The coaching focused on accurately applying labels and adherence to system instructions to decrease kick-outs." },
