cb0f870e186e9b56208db2f6056171c1ba6b35ca
Ivan Arias <arias.ivan@gmail.com>
2024-04-10 10:36:21 -0400

    active class added to the navBar, NavLink

--- src/components/NaviagtionBar.js
+++ src/components/NaviagtionBar.js
@@ -1,7 +1,8 @@
+import '../App.css';
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import LogOutButton from '../components/auth/LogOutButton'; // Make sure the path is correct
+import LogOutButton from '../components/auth/LogOutButton'; // Adjust the path as necessary
 
 const NavigationBar = () => {
   const [currentUser, setCurrentUser] = useState(null);
@@ -17,7 +18,7 @@ const NavigationBar = () => {
   return (
     <nav className="navbar navbar-expand-sm navbar-light bg-light rounded">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
+        <NavLink className="navbar-brand" to="/">
           {currentUser && currentUser.photoURL && (
             <img
               src={currentUser.photoURL}
@@ -26,24 +27,67 @@ const NavigationBar = () => {
             />
           )}
           {currentUser?.displayName || 'Guest'}
-        </Link>
+        </NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mx-auto">
             <li className="nav-item me-4">
-              <Link to="/" className="nav-link" style={{ fontSize: '14px', color: 'black', backgroundColor: 'orange', padding: '5px', borderRadius: '5px' }}>Audits</Link>
+              <NavLink
+                to="/"
+                className={({ isActive }) => isActive ? "nav-link nav-link-active" : "nav-link"}
+                style={({ isActive }) => ({
+                  fontSize: '14px',
+                  color: isActive ? 'white' : 'black', // Change text color when active
+                  backgroundColor: isActive ? 'green' : 'orange', // Change background color when active
+                  padding: '5px',
+                  borderRadius: '5px'
+                })}>
+                Audits
+              </NavLink>
             </li>
             <li className="nav-item me-3">
-              <Link to="/dashboard" className="nav-link" style={{ fontSize: '14px', color: 'black', backgroundColor: 'rgb(190, 253, 128)', padding: '5px', borderRadius: '5px' }}>Charts</Link>
+              <NavLink
+                to="/dashboard"
+                className={({ isActive }) => isActive ? "nav-link nav-link-active" : "nav-link"}
+                style={({ isActive }) => ({
+                  fontSize: '14px',
+                  color: isActive ? 'white' : 'black',
+                  backgroundColor: isActive ? 'blue' : 'rgb(190, 253, 128)',
+                  padding: '5px',
+                  borderRadius: '5px'
+                })}>
+                Charts
+              </NavLink>
             </li>
-
             <li className="nav-item me-4">
-              <Link to="/links" className="nav-link" style={{ fontSize: '14px', color: 'black', backgroundColor: 'rgb(220, 153, 178)', padding: '5px', borderRadius: '5px' }}>Links</Link>
+              <NavLink
+                to="/links"
+                className={({ isActive }) => isActive ? "nav-link nav-link-active" : "nav-link"}
+                style={({ isActive }) => ({
+                  fontSize: '14px',
+                  color: isActive ? 'white' : 'black',
+                  backgroundColor: isActive ? 'purple' : 'rgb(220, 153, 178)',
+                  padding: '5px',
+                  borderRadius: '5px'
+                })}>
+                Links
+              </NavLink>
             </li>
             <li className="nav-item me-4">
-              <Link to="/profile" className="nav-link" style={{ fontSize: '14px', color: 'black', backgroundColor: 'rgb(120, 253, 178)', padding: '5px', borderRadius: '5px' }}>Profile</Link>
+              <NavLink
+                to="/profile"
+                className={({ isActive }) => isActive ? "nav-link nav-link-active" : "nav-link"}
+                style={({ isActive }) => ({
+                  fontSize: '14px',
+                  color: isActive ? 'white' : 'black',
+                  backgroundColor: isActive ? 'red' : 'rgb(120, 253, 178)',
+                  padding: '5px',
+                  borderRadius: '5px'
+                })}>
+                Profile
+              </NavLink>
             </li>
           </ul>
           {currentUser && (
@@ -63,105 +107,3 @@ export default NavigationBar;
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* import React, { useState } from 'react';
-import LogOutButton from './LogOutButton'; // Ensure this path is correct
-
-import { Link } from 'react-router-dom';
-
-import {
-    MDBNavbar,
-    MDBContainer,
-    MDBIcon,
-    MDBNavbarNav,
-    MDBNavbarItem,
-    MDBNavbarLink,
-    MDBNavbarToggler,
-    MDBNavbarBrand,
-    MDBCollapse,
-    MDBBtn
-} from 'mdb-react-ui-kit';
-
-export default function App() {
-    const [openNavColor, setOpenNavColor] = useState(false);
-
-
-    return (
-        <>
-            <MDBNavbar expand='lg' dark bgColor='primary'>
-                <MDBContainer fluid>
-                    <MDBNavbarBrand href='#'>Navbar</MDBNavbarBrand>
-                    <MDBNavbarToggler
-                        type='button'
-                        data-target='#navbarColor02'
-                        aria-controls='navbarColor02'
-                        aria-expanded='false'
-                        aria-label='Toggle navigation'
-                        onClick={() => setOpenNavColor(!openNavColor)}
-                    >
-                        <MDBIcon icon='bars' fas />
-                    </MDBNavbarToggler>
-                    <MDBCollapse open={openNavColor} navbar>
-                        <MDBNavbarNav className='me-auto mb-2 mb-lg- justify-content-center'>
-                            <MDBNavbarItem className='active'>
-
-                            </MDBNavbarItem>
-                            <MDBNavbarItem>
-                                <MDBBtn size='lg' color='warning'><Link to="/">Audits</Link></MDBBtn>
-                            </MDBNavbarItem>
-
-                            <MDBNavbarItem>
-                           
-                                <MDBBtn size='lg' color='secondary'><Link to="/dashboard">Charts</Link></MDBBtn>
-                            </MDBNavbarItem>
-                            <MDBNavbarItem>
-                                <LogOutButton />
-                            </MDBNavbarItem>
-                        </MDBNavbarNav>
-                    </MDBCollapse>
-                </MDBContainer>
-            </MDBNavbar>
-
-            <br />
-
-
-
-            <br />
-
-
-        </>
-    );
-}
-
-
-
- */
